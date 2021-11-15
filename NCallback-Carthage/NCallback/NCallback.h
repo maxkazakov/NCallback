3e4475a151362176f5a3f21c06d47d57913a0830
Max Kazakov <kazakov.maixm.a@gmail.com>
2021-11-15 19:18:29 +0500

    carthage support

---
+++ NCallback-Carthage/NCallback/NCallback.h
@@ -0,0 +1,18 @@
+//
+//  NCallback.h
+//  NCallback
+//
+//  Created by Максим Казаков on 15.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NCallback.
+FOUNDATION_EXPORT double NCallbackVersionNumber;
+
+//! Project version string for NCallback.
+FOUNDATION_EXPORT const unsigned char NCallbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NCallback/PublicHeader.h>
+
+
---
+++ NCallback-Carthage/NCallbackTestHelpers/NCallbackTestHelpers.h
@@ -0,0 +1,18 @@
+//
+//  NCallbackTestHelpers.h
+//  NCallbackTestHelpers
+//
+//  Created by Максим Казаков on 15.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NCallbackTestHelpers.
+FOUNDATION_EXPORT double NCallbackTestHelpersVersionNumber;
+
+//! Project version string for NCallbackTestHelpers.
+FOUNDATION_EXPORT const unsigned char NCallbackTestHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NCallbackTestHelpers/PublicHeader.h>
+
+
